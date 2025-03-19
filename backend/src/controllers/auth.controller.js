3a0de4659fb68c5e42769752c9df019745d2ea37
Tokosama <anouartoko@gmail.com>
2025-03-19 14:50:08 +0100

    fix of importation issues and add of the check routes plus the checkAuth controller to return the login user

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,7 +2,6 @@ import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
-import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -112,3 +111,14 @@ export const updateProfile = async (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
+export const checkAuth = async (req,res) =>{
+
+  try{
+    res.status(200).json(req.user);
+
+  }catch(error){
+    console.log("Error in checkAuth controller", error.message);
+    res.status(500).json({message: "Internal Server Error"});
+  }
+}
