3dd7b15e77677636099c0ab8b298d2d3c7ee3051
Tokosama <anouartoko@gmail.com>
2025-03-21 23:44:03 +0100

    Add of the chat to chat endpoint and implementation of the getMessages functionnality ,

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware";
-import { getUsersForSidebar } from "../controllers/message.controller";
+import { getUsersForSidebar,getMessages } from "../controllers/message.controller";
 
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/:id",protectRoute,getMessages)
 
 export default router;
