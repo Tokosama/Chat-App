0e894cdf2cd733c50ad4978b5c655e370a5f1664
Tokosama <anouartoko@gmail.com>
2025-03-23 11:04:00 +0100

    Initialisation of the different  pages, and setting up of Routes in App.jsx plus add of the axios, and zustand dependenciies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,38 @@
+import { Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { HomePage } from "./pages/HomePage";
+import { SignUpPage } from "./pages/SignUpPage";
+import { LoginPage } from "./pages/LoginPage";
+import { SettingsPage } from "./pages/SettingsPage";
+import { ProfilePage } from "./pages/ProfilePage";
+
 function App() {
   return (
     <>
       <div className="bg-red-400">
-      
+        <Navbar />
+        <Routes>
+          <Route
+            path="/"
+            element={<HomePage />}
+          />
+          <Route
+            path="/signup"
+            element={<SignUpPage />}
+          />
+          <Route
+            path="/login"
+            element={<LoginPage />}
+          />
+          <Route
+            path="/settings"
+            element={<SettingsPage />}
+          />
+           <Route
+            path="/profile"
+            element={<ProfilePage />}
+          />
+        </Routes>
       </div>
     </>
   );
