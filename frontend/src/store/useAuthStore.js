9ca30d7e68ba76bcf1dae5095008600d5c934226
Tokosama <anouartoko@gmail.com>
2025-03-25 10:24:11 +0100

    Add of the ProfilePage and the update profile image functionnality

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -32,26 +32,39 @@ export const userAuthStore = create((set) => ({
       set({ isSigningUp: false });
     }
   },
-  login:async(data) =>{
-    set({isLoggingIn:true});
-    try{
+  login: async (data) => {
+    set({ isLoggingIn: true });
+    try {
       const res = await axiosInstance.post("/auth/login", data);
-      set({authUser:res.data});
+      set({ authUser: res.data });
       toast.success("LOgged in successfully");
-    }catch(error){
+    } catch (error) {
       toast.error(error.response.data.message);
-    }finally{
-      set({isLoggingIn:false})
+    } finally {
+      set({ isLoggingIn: false });
     }
-     
   },
   logout: async () => {
     try {
       await axiosInstance.post("/auth/logout");
       set({ authUser: null });
-      toast.success("Logged out sucessfully")
+      toast.success("Logged out sucessfully");
     } catch (error) {
       toast.error(error.response.data.message);
+    }
+  },
+
+  updateProfile: async (data) => {
+    set({ isUpdatingProfile: true });
+    try {
+      const res = await axiosInstance.put("/auth/update-profile", data);
+      set({ authUser: res.data });
+      toast.success("Profile updated successfully");
+    } catch (error) {
+      console.log("Error in update profile", error);
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isUpdatingProfile: false });
     }
   },
 }));
